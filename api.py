f2b65ee413bfa774b51b05133b18b643dc7d7fe9
Levin <artofnoises0@gmail.com>
2019-12-04 17:52:02 +0300

    Added routes for paths of application

--- api.py
+++ api.py
@@ -2,10 +2,43 @@ from webob import Request, Response
 
 
 class API:
+	def __init__(self):
+		# self.routes - dict for paths our application
+		self.routes = {}
+
+
 	def __call__(self, environ, start_response):
 		request = Request(environ)
 
-		response = Response()
-		response.text = "Hello, World!"
+		response = self.handle_request(request)
 
 		return response(environ, start_response)
+
+	def handle_request(self, request):
+		response = Response()
+
+		handler = self.find_handler(request_path=request.path)
+
+		if handler is not None:
+			handler(request, response)
+		else:
+			self.default_response(response)
+		
+		return response
+
+	def route(self, path):
+		def wrapper(handler):
+			self.routes[path] = handler
+			return handler
+
+		return wrapper
+
+	def default_response(self, response):
+		response.status_code = 404
+		response.text = "Not found."
+
+
+	def find_handler(self, request_path):
+		for path, handler in self.routes.items():
+			if path == request_path:
+				return handler
--- app.py
+++ app.py
@@ -1,3 +1,13 @@
 from api import API
 
 app = API()
+
+@app.route("/home")
+def home(request, response):
+	response.text = "Hello from HOME page"
+
+
+@app.route("/about")
+def about(request, response):
+	response.text = "Hello from ABOUT page"
+
